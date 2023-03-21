359f0124409650b36415b8e9a68a6b8054ef3071
Greka <kkdaniilb@yandex.ru>
2023-03-21 22:15:45 +0300

    Laba 1-4

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,20 +1,59 @@
 package ru.iu3.fclient;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'fclient' library on application startup.
+public class MainActivity extends AppCompatActivity{
+    private ActivityMainBinding binding;
+
+    // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("fclient");
+        System.loadLibrary("native-lib");
+        System.loadLibrary("mbedcrypto");
     }
 
-    private ActivityMainBinding binding;
+    // Метод, который возвращает название сайта
+    protected String getPageTitle(String html) {
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find())
+            p = matcher.group(1);
+        else
+            p = "Not found";
+        return p;
+    }
+
+    // Метод, который тестирует работу http клиента
+    protected void testHttpClient() {
+        new Thread(() -> {
+            try {
+                HttpURLConnection uc = (HttpURLConnection) (new URL("http://10.0.2.2:8081/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() -> {
+                    Toast.makeText(this, title, Toast.LENGTH_LONG).show();
+                });
+            } catch (Exception exception) {
+                Log.e("fapptag", "Http client fails", exception);
+            }
+        }).start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +62,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        Button myButton = (Button) findViewById(R.id.sample_button);
     }
 
-    /**
-     * A native method that is implemented by the 'fclient' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    // Альтернативный метод нажатия кнопки
+    public void onButtonClick(View view) {
+        testHttpClient();
+    }
 }
-
